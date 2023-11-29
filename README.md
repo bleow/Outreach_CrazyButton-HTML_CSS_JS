3ff2648edbb8af826c46d1d30fb7d620612d8b29
BryanLeow <bryankhleow@gmail.com>
2023-11-30 03:33:33 +0800

    feat: organised code and verified it works

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# OutreachWrkshp_CrazyButton-HTML_CSS_JS
- SCSE OUTREACH WORKSHOP AY21/22 SEM 1 | Crazy Button (Web programming)
+# SCSE Outreach Workshop - AY23/23 Sem 1 Run
+
+Crazy Button (Web Programming)
+- HTML
+- Tailwind CSS
+- Python Flask
+- Websockets
+
+Previous run: AY21/22 Sem 1
+
+----
+
+`cd [directory]`
+
+`pip install aiohttp`
+
+`pip install python-socketio`
+
+`python server.py`
+
+https://stackoverflow.com/questions/63385158/installing-socketio-module-on-python3-seems-to-be-corrupting-pip
--- instructors-copy/server.py
+++ instructors-copy/server.py
@@ -1,23 +1,31 @@
-### server.py ###
-# import libraries that will help us with creating the server
-import socketio
-from aiohttp import web
+# IMPORT // libraries that will help us with creating the backend server
+import os
 from random import uniform
+from aiohttp import web
+import socketio
 
-# create a Socket.IO server
+
+# INITIALISATION // create a web app, web socket, and bind the web app to the web socket
 sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
 
+
 async def index(request):
-    with open('C:\\.PROGRAMMING\\HTML_CSS_JS\\OutreachWrkshp_CrazyButton-HTML_CSS_JS\\index.html') as f:
+    """
+    (coroutine) Request handler to serve our webpage
+    :param request: Request instance
+    :return: StreamResponse-derived (e.g. Response) instance
+    """
+    CUR_DIRECTORY = os.getcwd()
+    INDEX_DIRECTORY = CUR_DIRECTORY + '\\index.html'
+    with open(INDEX_DIRECTORY) as f:
         return web.Response(text=f.read(), content_type='text/html')
 
 
 ## We bind our aiohttp endpoint to our app
 ## router
 app.router.add_get('/', index)
-
 
 # keep track of list of connected players
 # {  player1_sid: score,
